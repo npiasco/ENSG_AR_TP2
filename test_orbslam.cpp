75101fdd1345483ccf363e9744728e0c24db3c0a
Nathan Piasco <nathan.piasco@gmail.com>
2018-01-18 09:20:16 +0100

    test orbslam

---
+++ test_orbslam.cpp
@@ -0,0 +1,72 @@
+/**
+* This file is part of ORB-SLAM2.
+*
+* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
+* For more information see <https://github.com/raulmur/ORB_SLAM2>
+*
+* ORB-SLAM2 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ORB-SLAM2 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+
+#include<opencv2/core/core.hpp>
+
+#include<System.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	if(argc != 3) 
+	{
+		cerr << endl << "Usage: ./path_to_PF_ORB path_to_vocabulary path_to_settings" << endl;
+		return 1;
+	}
+	cv::VideoCapture cap(0);
+	if (!cap.isOpened())
+	{
+		cerr << endl  <<"Could not open camera feed."  << endl;
+		return -1;
+	}
+	// Create SLAM system. It initializes all system threads and gets ready to process frames.
+	ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
+
+	cout << endl << "-------" << endl;
+	cout << "Start processing sequence ..." << endl;
+
+	// Main loop
+	int timeStamps=0;
+
+	for(;;timeStamps++)
+	{
+		//Create a new Mat
+		cv::Mat frame;
+
+		//Send the captured frame to the new Mat
+		cap>>frame;
+
+		// Pass the image to the SLAM system
+		auto pose = SLAM.TrackMonocular(frame, timeStamps);
+	}
+
+	// Stop all threads
+	SLAM.Shutdown();
+
+	// Save camera trajectory
+	SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+	return 0;
+}
